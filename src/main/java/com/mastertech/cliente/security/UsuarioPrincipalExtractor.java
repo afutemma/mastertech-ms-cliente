dc129e99b8234ca2c0b275480ee3955e0c51b7f6
A2W <a2w@localhost.localdomain>
2020-08-07 17:29:51 -0300

    add service config com oauth2 e feign

---
+++ src/main/java/com/mastertech/cliente/security/UsuarioPrincipalExtractor.java
@@ -0,0 +1,19 @@
+package com.mastertech.cliente.security;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
+
+import java.util.Map;
+
+public class UsuarioPrincipalExtractor  implements PrincipalExtractor {
+
+    @Override
+    public Object extractPrincipal(Map<String, Object> map) {
+        Usuario usuario = new Usuario();
+
+        usuario.setNome(map.get("name").toString());
+        usuario.setId((Integer) map.get("id"));
+
+        return usuario;
+    }
+
+}
